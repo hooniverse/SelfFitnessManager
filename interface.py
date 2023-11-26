452fa4796c4a7d4e0e8299ea21c831eeb1a76f67
egaahhh <cndl03@jnu.ac.kr>
2023-11-26 21:27:14 +0900

    오류 수정

--- interface.py
+++ interface.py
@@ -76,17 +76,4 @@ class Interface:
         window.close()
         return user_input
 
-    def run(self):
-        layout = [[sg.Graph((250, 280), (0, 0), (250, 280), key='logo')],
-                  [sg.Text('모드선택', font=("Helvetica", 10, "bold")), sg.Radio('트레이닝', 1), sg.Radio('테스트', 1)],
-                  [sg.Button('확인')]]
-        window = sg.Window('모드 선택', layout, finalize=True)
-        window['logo'].draw_image(filename='logo1.png', location=(6.5, 270))
 
-        while True:
-            event, values = window.read()
-            if event == sg.WIN_CLOSED or event == 'Cancel':
-                break
-            print('You entered', values[0])
-
-        window.close()
--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 import interface
-from training import Training
+# from training import Training
 
-# interface = interface.Interface() ##인터페이스 객체 생성
+interface = interface.Interface() ##인터페이스 객체 생성
 #
-# user_input = interface.run() ##인터페이스 run함수 -- return으로 리스트
-#[training, 종목, set, set당 개수, 쉬는시간] / [test, 종목, 목표시간, 목표개수]
+user_input = interface.run() ##인터페이스 run함수 -- return으로 리스트
+# [training, 종목, set, set당 개수, 쉬는시간] / [test, 종목, 목표시간, 목표개수]
 user_input = [0, 0, 1, 1, 2]
 
 if user_input[0] == 0:#training인 경우
