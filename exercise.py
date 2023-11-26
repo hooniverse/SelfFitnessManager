7011d81674d2d4a1d5e90fafdd9731612fcb2021
Ryu <yhs9910@gmail.com>
2023-11-26 18:38:41 +0900

    Exercise클래스 생성자

---
+++ exercise.py
@@ -0,0 +1,16 @@
+from gtts import gTTS
+from playsoud import playsound
+
+class Exercise:
+    def __init__(self, type):
+        self.type = type ##운동종목 정수로 판별
+        self.count = 0
+        self.status = True
+
+
+
+
+
+
+
+
